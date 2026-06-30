52edc980844b39e09ef1f98783348732c9654ccd
namang142-blip <namang142@gmail.com>
2026-06-30 19:23:43 +0530

    Refactor wire declarations and monitoring in testbench
    Refactor testbench to declare HL and SL wires together and update monitoring.

--- TLC_tb.v
+++ TLC_tb.v
@@ -1,8 +1,8 @@
 module testbench;
   reg clk,rst;
-  wire [1:0] HL;
-  wire [1:0] SL;
-  TLC dut(   
+  wire [1:0] HL,SL;
+  
+  TLC dut(
     .clk(clk),
     .rst(rst),
     .HL(HL),
@@ -14,19 +14,17 @@ module testbench;
     clk=0;
     rst=1;
     
-    $dumpfile("dump.vcd");
-    $dumpvars(0,testbench);
-    
+    $monitor("t=%0t -> HL=%b -> SL=%b",$time,HL,SL);
+
     #10 rst=0;
     @(posedge clk);
     
     for(i=0;i<20;i=i+1)begin
       @(posedge clk);
-      $display("t=%0t -> HL=%b -> SL=%b",$time,HL,SL);
     end
     $finish;
   end
 endmodule
     
     
-  
+    
